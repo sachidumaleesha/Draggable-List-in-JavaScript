a318505cdcef11bd862cfa77cda617465f4bd11a
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-02-26 08:08:19 +0530

    Add JavaScript to enable drag the list items

---
+++ App.js
@@ -0,0 +1,25 @@
+const sortableList = document.querySelectorAll(".sortable-list");
+const items = document.querySelectorAll(".item");
+
+items.forEach(item => {
+    item.addEventListener("dragstart", () => {
+        setTimeout(() => item.classList.add("dragging"), 0);
+    })
+
+    item.addEventListener("dragend", () => item.classList.remove("dragging"));
+});
+
+const initSortableList = (e) =>{
+    e.preventDefault();
+    const draggingItem = sortable.querySelector(".dragging");
+    const siblings = [...sortableList.querySelectorAll(".item:not(.dragging)")];
+
+    let nextSibling = siblings.find(sibling => {
+        return e.clientY <= sibling.offsetTop + sibling.offsetHeight / 2;
+    });
+
+    sortableList.insertBefore(draggingItem, nextSibling);
+}
+
+sortableList.addEventListener("dragover", initSortableList);
+sortableList.addEventListener("dragenter", e => e.preventDefault());
